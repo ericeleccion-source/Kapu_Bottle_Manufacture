4b6441c6763007adb44fe83153519d6a8678b75f
Eric Eleccion <ericeleccion@gmail.com>
2025-09-03 20:56:09 -0700

    build: convert PostCSS/Tailwind config to ESM

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,7 @@
-﻿module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } };
+﻿/** @type {import('postcss-load-config').Config} */
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 ﻿/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: { extend: {} },
-  darkMode: 'class',
-  plugins: []
+  plugins: [],
 };
